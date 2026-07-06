b0140e2b144c5b9ab91d424b099b053da608cc28
Mathys <38434126+mathysth@users.noreply.github.com>
2026-07-06 21:55:46 +0200

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score = []
+        
+        for operation in operations:
+            match operation:
+                case 'C':
+                    score.pop()
+                case '+':
+                    score.append(score[-1] + score[-2])
+                case 'D':
+                    score.append(score[-1] * 2)
+                case _:
+                    score.append(int(operation))
+
+        return sum(score)
